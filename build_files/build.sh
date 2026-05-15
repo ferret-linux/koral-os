8cfe3e358ef9ba647ef9ba1f4bfe7c00456a06fb
ferret#dev <ferret.linux@gmail.com>
2026-05-15 12:48:53 +0530

    Add : initramfs logic from bluebuild

--- build_files/build.sh
+++ build_files/build.sh
@@ -133,7 +133,36 @@ sed -i 's/^NAME=.*/NAME="KoralOS"/' /usr/lib/os-release
 sed -i 's/^PRETTY_NAME=.*/PRETTY_NAME="KoralOS Linux"/' /usr/lib/os-release
 
 ### Rebuild initramfs
-dracut --no-hostonly --kver "$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}\n' | tail -1)" -f
+if [ -f "/usr/libexec/rpm-ostree/wrapped/dracut" ]; then
+    DRACUT="/usr/libexec/rpm-ostree/wrapped/dracut"
+else
+    DRACUT="/usr/bin/dracut"
+fi
+
+temp_conf_file="$(mktemp '/etc/dracut.conf.d/zzz-loglevels-XXXXXXXXXX.conf')"
+cat >"${temp_conf_file}" <<'EOF'
+stdloglvl=4
+sysloglvl=0
+kmsgloglvl=0
+fileloglvl=0
+EOF
+
+for kernel_path in /usr/lib/modules/*/; do
+    kernel_path="${kernel_path%/}"
+    initramfs_image="${kernel_path}/initramfs.img"
+    qual_kernel="${kernel_path##*/}"
+    echo "Rebuilding initramfs for kernel: ${qual_kernel}"
+    "${DRACUT}" \
+        --kver "${qual_kernel}" \
+        --force \
+        --add 'ostree' \
+        --no-hostonly \
+        --reproducible \
+        "${initramfs_image}"
+    chmod 0600 "${initramfs_image}"
+done
+
+rm -- "${temp_conf_file}"
 
 ### Cleanup
 dnf config-manager setopt ferret-pkgs.enabled=0
