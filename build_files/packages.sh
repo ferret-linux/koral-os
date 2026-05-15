a4813df916e821e4db889be2c08811cdd631d253
ferret#dev <ferret.linux@gmail.com>
2026-05-15 13:58:42 +0530

    Fix : remove double cleanup

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -118,10 +118,3 @@ dnf5 install -y --setopt=install_weak_deps=False \
 ### Enable system services
 systemctl enable plasmalogin.service
 systemctl enable switcheroo-control.service
-
-### Cleanup
-dnf config-manager setopt ferret-pkgs.enabled=0
-rm -f /etc/yum.repos.d/ferret-pkgs.repo
-dnf5 autoremove -y
-dnf5 clean all
-dnf5 clean packages
